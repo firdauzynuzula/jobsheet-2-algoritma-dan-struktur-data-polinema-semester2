eb1da50c4ced93a62fc2009737d882dd0a9f350b
firdauzynuzula <nuzulafirdhauzy@gmail.com>
2026-02-23 02:00:32 +0700

    push file Mahasiswa11.java

---
+++ Mahasiswa11.java
@@ -0,0 +1,39 @@
+public class Mahasiswa11 {
+    
+
+    class Mahasiswa {
+        String nama;
+        String nim;
+        String kelas;
+        double ipk;
+
+        void tampilkanInformasi() {
+            System.out.println("Nama: " + nama);
+            System.out.println("Nim: " + nim);
+            System.out.println("IPK: " + kelas);
+            System.out.println("Kelas: " + ipk);
+        }
+
+        void ubahKelas(String kelasBaru) {
+            kelas = kelasBaru;
+        }
+
+        void updateIPK(double ipkBaru) {
+            ipk = ipkBaru;
+        }
+
+        String nilaiKinerja() {
+            if (ipk >= 3.5) {
+                return "Kinerja sangat baik";
+            } else if (ipk >= 3.0) {
+                return "Kinerja Baik";
+            }
+                else if (ipk >= 2.0) {
+                return "Kinerja cukup";
+            }
+                else {
+                return "Kinerja kurang";
+            }
+        }
+    }
+}
